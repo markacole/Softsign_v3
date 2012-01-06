fd7b89b8b33ee4e71c3cce010c7c337f1d9e71bf
Mark Cole <mark.a.cole@me.com>
2012-01-06 08:56:05 +0000

    Added New Project

---
+++ Softsign_V3/Softsign_V3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Softsign_V3
+//
+//  Created by Mark Cole on 06/01/2012.
+//  Copyright (c) 2012 SeriouslyRevival. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SS3AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SS3AppDelegate class]));
+    }
+}
